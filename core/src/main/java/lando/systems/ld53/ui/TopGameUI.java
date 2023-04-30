f987e1c908662a18abc65d54fd10c97263320c15
Jeffrey Hwang <jjhwang@wisc.edu>
2023-04-30 11:14:14 -0500

    temporary resizing of progressbar before remake

--- core/src/main/java/lando/systems/ld53/ui/TopGameUI.java
+++ core/src/main/java/lando/systems/ld53/ui/TopGameUI.java
@@ -11,17 +11,11 @@ import lando.systems.ld53.screens.GameScreen;
 import lando.systems.ld53.utils.Utils;
 
 public class TopGameUI extends Group {
-    private final float UI_WIDTH = 600f;
-    private final float UI_HEIGHT = 75f;
+    private final float UI_WIDTH = 300f;
+    private final float UI_HEIGHT = 105f;
     private VisProgressBar staminaBar;
 
     public TopGameUI(GameScreen screen) {
-        VisWindow window = new VisWindow("");
-        window.background(Assets.Patch.glass.drawable);
-        window.setPosition(Config.Screen.window_width / 2f - UI_WIDTH / 2f , Config.Screen.window_height - UI_HEIGHT);
-        window.setSize(UI_WIDTH, UI_HEIGHT);
-        addActor(window);
-
         VisProgressBar.ProgressBarStyle horizontalProgressBarStyle = screen.skin.get("default-horizontal", VisProgressBar.ProgressBarStyle.class);
         VisProgressBar.ProgressBarStyle staminaBarStyle = new VisProgressBar.ProgressBarStyle(horizontalProgressBarStyle);
         staminaBarStyle.knobAfter =  new TextureRegionDrawable(Utils.getColoredTextureRegion(Color.YELLOW));
@@ -29,9 +23,10 @@ public class TopGameUI extends Group {
         staminaBar = new VisProgressBar(0f, 100f, .05f, false);
         staminaBar.setValue(100f);
         staminaBar.setStyle(staminaBarStyle);
-        staminaBar.setPosition(30f, 2.5f);
-        staminaBar.setSize(UI_WIDTH - 60f, UI_HEIGHT);
-        window.addActor(staminaBar);
+        staminaBar.setPosition(Config.Screen.window_width / 2f - UI_WIDTH / 2f, Config.Screen.window_height - UI_HEIGHT);
+        staminaBar.setSize(UI_WIDTH, UI_HEIGHT);
+        staminaBar.setHeight(UI_HEIGHT);
+        addActor(staminaBar);
     }
 
     public void update(float staminaPercentage) {
