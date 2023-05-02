420dec83ebd20ad4890fac845e82d1f5f2e1b46c
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2023-05-01 19:50:34 -0500

    Update ability description text

--- core/src/main/java/lando/systems/ld53/entities/PlayerAbility.java
+++ core/src/main/java/lando/systems/ld53/entities/PlayerAbility.java
@@ -4,11 +4,11 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public enum PlayerAbility {
 
-    shield_360   (4f,  "Shield",  "Block It!",         true),
-    bomb_throw   (3f,  "Bomb",    "    Blast!",        true),
-    speed_up     (5f,  "Speed",   "Personal space...", true),
-    repulse      (6f,  "Repulse", "Get away, please.", true),
-    fetch        (10f, "Fetch",   "Good pupper->",     true)
+    shield_360   (4f,  "Shield",  "Block it!",            true),
+    bomb_throw   (3f,  "Bomb",    "Big bada-boom",        true),
+    speed_up     (5f,  "Speed",   "Gotta go fast!",       true),
+    repulse      (6f,  "Repulse", "Enforce your personal space",      true),
+    fetch        (10f, "Fetch",   "Good puppers help fetch packages", true)
     ;
 
     // textureRegion is being set in assets class.
